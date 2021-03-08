36dd149294a12fc786c7363e098a08abfe219369
ucchiii0210 <y09052842409@icloud.com>
2021-03-08 23:26:41 +0900

    登録機能完了

--- db/migrate/20210308110053_devise_create_users.rb
+++ db/migrate/20210308110053_devise_create_users.rb
@@ -7,12 +7,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      t.string :nickname,           null: false
-      t.string :first_name,         null: false
-      t.string :last_name,          null: false
-      t.string :first_name_kana,    null: false
-      t.string :last_name_kana,     null: false
-      t.string :birthday,           null: false
+      t.string :nickname
+      t.string :first_name
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.date   :birthday
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 2021_03_08_110053) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "last_name_kana", null: false
-    t.string "birthday", null: false
+    t.string "nickname"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "first_name_kana"
+    t.string "last_name_kana"
+    t.date "birthday"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
