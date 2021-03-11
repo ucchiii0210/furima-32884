1d895e903857142d6177e933cb6d53d50e3474ec
ucchiii0210 <y09052842409@icloud.com>
2021-03-11 18:59:33 +0900

    user登録テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
+         validates :nickname, presence: true 
          validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
          validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
          validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
