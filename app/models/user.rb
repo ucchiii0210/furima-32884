a2699415d074d0985cc6b8ed41e776519b1083a0
ucchiii0210 <y09052842409@icloud.com>
2021-03-11 20:03:11 +0900

    レビューの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         validates :nickname, presence: true 
-         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-         validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-         validates :birthday, presence: true
-
-         validates :password,format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])\w{6,100}\z/}
-         
+          
          has_many :items
          has_many :purchases
+
+         validates :password,format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])\w{6,100}\z/}
+
+         with_options presence: true do
+         validates :nickname 
+         validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/}
+         validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/}
+         validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+         validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+         validates :birthday
+         end
 end
