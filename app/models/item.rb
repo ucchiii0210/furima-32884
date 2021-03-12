ab28b30dea1c268469cba7cd8172d84275e15f5c
ucchiii0210 <y09052842409@icloud.com>
2021-03-12 20:40:51 +0900

    画像添付機能(アクティブストレージ)

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,19 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one    :purchase
+  has_one_attached :image
   
   with_options presence: true do
     validates :image
     validates :product        
-    validates :text         
-    validates :category_id  
-    validates :sipping_id   
-    validates :status_id  
-    validates :area_id 
-    validates :day_id  
-    validates :price 
+    validates :text 
+    with_options numericality: { only_integer: true } do
+      validates :category_id  
+      validates :sipping_id   
+      validates :status_id  
+      validates :area_id 
+      validates :day_id  
+      validates :price 
+    end
   end
 end
