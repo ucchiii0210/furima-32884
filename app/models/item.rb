14e073f8a7ef2a0764bff37a7ea6a360ecf19249
ucchiii0210 <y09052842409@icloud.com>
2021-03-12 19:50:11 +0900

    バリデーションとマイグレーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one    :purchase
+  
+  with_options presence: true do
+    validates :image
+    validates :product        
+    validates :text         
+    validates :category_id  
+    validates :sipping_id   
+    validates :status_id  
+    validates :area_id 
+    validates :day_id  
+    validates :price 
+  end
 end
