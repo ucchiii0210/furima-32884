05db77f068bf42729eb365fb6cc5799cac84f06d
ucchiii0210 <y09052842409@icloud.com>
2021-03-14 16:18:03 +0900

    java手数料計算

---
+++ app/javascript/item_price.js
@@ -0,0 +1,16 @@
+
+window.addEventListener('load', () => {
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   const tax = 0.1
+   const fee = inputValue * tax
+   const profit = inputValue - fee
+   const taxArea = document.getElementById("add-tax-price");
+   const profitArea = document.getElementById("profit");
+   taxArea.innerHTML = fee;
+   profitArea.innerHTML = profit;
+  })
+});
+
+
