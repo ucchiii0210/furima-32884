e1b13d7787ebdf497fe48be6499eea028313f8df
ucchiii0210 <y09052842409@icloud.com>
2021-03-18 21:30:21 +0900

    削除機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! , except:  [:index ,:show]
   before_action :move_to_index ,only: [:edit , :update]
-  before_action :set_item , only: [:show, :edit, :update ]
+  before_action :set_item , only: [:show, :edit, :update]
 
   def index
     @items = Item.all
@@ -34,7 +34,11 @@ class ItemsController < ApplicationController
     end
   end
    
-
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
 
 
   private
