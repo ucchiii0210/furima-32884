ad384a13714b913624ce03dc6576519855945f55
ucchiii0210 <y09052842409@icloud.com>
2021-03-18 21:43:15 +0900

    レビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! , except:  [:index ,:show]
+  before_action :set_item , only: [:show, :edit, :update ,:destroy]
   before_action :move_to_index ,only: [:edit , :update]
-  before_action :set_item , only: [:show, :edit, :update]
-
+ 
   def index
     @items = Item.all
   end
@@ -35,8 +35,7 @@ class ItemsController < ApplicationController
   end
    
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
@@ -46,15 +45,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product, :image, :text, :category_id, :status_id, :sipping_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+  def set_item
     @item = Item.find(params[:id])
+  end
+  def move_to_index
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
-
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
 end
