b67d8cd041d4d37dfbfc6c120b9302b270643307
ucchiii0210 <y09052842409@icloud.com>
2021-04-10 20:42:51 +0900

    プレビュー機能完成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../card")
+require('../preview') 
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
